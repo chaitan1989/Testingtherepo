96bccace7428f1a0389fd4365e100b7032169eeb
chaitanyagit <chaitanya.srinivas14@gmail.com>
2017-10-22 18:49:24 +0530

    Added 9 and 10 files

---
+++ src/javapractice/Classdemo10.java
@@ -0,0 +1,24 @@
+package javapractice;
+
+public class Classdemo10 {
+	
+	int x =910;
+	int y =120;
+
+	public static void main(String[] args) {
+		
+		Classdemo10 obj1 = new Classdemo10();
+		obj1.sum();
+		System.out.println("sum is " +obj1.x);
+		System.out.println("i am learning automation");
+
+	}
+	
+	public void sum(){
+		int a = 50;
+		int b = 90;
+		int c = a + b;
+		System.out.println("addition of two numbers is " +c);
+	}
+
+}
---
+++ src/javapractice/Classdemo9.java
@@ -0,0 +1,24 @@
+package javapractice;
+
+public class Classdemo9 {
+	
+	int x =910;
+	int y =120;
+
+	public static void main(String[] args) {
+		
+		Classdemo9 obj1 = new Classdemo9();
+		obj1.sum();
+		System.out.println("sum is " +obj1.x);
+		System.out.println("i am learning automation");
+
+	}
+	
+	public void sum(){
+		int a = 50;
+		int b = 90;
+		int c = a + b;
+		System.out.println("addition of two numbers is " +c);
+	}
+
+}
