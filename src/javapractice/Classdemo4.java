7451d4890cf7c6a1e5d54a21c54cc5fb4d05d69a
chaitanyagit <chaitanya.srinivas14@gmail.com>
2017-10-22 18:44:01 +0530

    New Java file class demo 4 added

---
+++ src/javapractice/Classdemo4.java
@@ -0,0 +1,24 @@
+package javapractice;
+
+public class Classdemo4 {
+	
+	int x =910;
+	int y =120;
+
+	public static void main(String[] args) {
+		
+		Classdemo4 obj1 = new Classdemo4();
+		obj1.sum();
+		System.out.println("sum is " +obj1.x);
+		System.out.println("i am learning automation");
+
+	}
+	
+	public void sum(){
+		int a = 30;
+		int b = 40;
+		int c = a + b;
+		System.out.println("addition of two numbers is " +c);
+	}
+
+}
